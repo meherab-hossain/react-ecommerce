30bb4dddffbbde1b4be5a9976b6861752183e46d
Md. Meherab Hossain <h.meherabhossain@gmail.com>
2022-06-14 12:12:17 +0600

    newsletter added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import Slider from "../components/slider/Slider";
 import Categories from "../components/categories/Categories";
 import Products from "../components/products/Products";
+import Newsletter from "../components/Newsletter";
 const Home = () => {
   return (
     <div>
@@ -12,6 +13,7 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products/>
+      <Newsletter/>
     </div>
   );
 };
