10661c22095d781c07c3e5a4a81995cdf2e8b798
Md. Meherab Hossain <h.meherabhossain@gmail.com>
2022-06-15 13:24:04 +0600

    login and register page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
 
 function App() {
-  return <Product />;
+  return <Register />;
 }
 
 export default App;
